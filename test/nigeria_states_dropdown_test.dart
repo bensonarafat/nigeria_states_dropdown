13462410b32eb677f5200cdd0b3020f2ab0c44a9
Benson Arafat <benpaul320@gmail.com>
2022-09-18 16:05:08 +0100

    License & ChangeLog

--- test/nigeria_states_dropdown_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:nigeria_states_dropdown/nigeria_states_dropdown.dart';
-
-void main() {
-  test('adds one to input values', () {});
-}
